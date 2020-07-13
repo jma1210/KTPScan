fdfc6fb6500fe496634b1e66a2304aa58da97348
Joszef <jmadiguna@connect.ust.hk>
2020-07-13 16:16:44 +0700

    LocFields now sorts the information extracted from the KTP card

--- app/src/main/java/com/example/cv_ocr/LocFields.java
+++ app/src/main/java/com/example/cv_ocr/LocFields.java
@@ -4,9 +4,17 @@ import com.google.android.gms.vision.text.Line;
 
 import java.util.ArrayList;
 
+/**
+ * Trying out location based filtering
+ * Members are :
+ * 1. Lines - ArrayList of lines object gotten from ML OCR process
+ * 2. InfoFields 2D ArrayList of String type object, meant to simulate placement of KTP information
+ */
 public class LocFields {
     ArrayList<Line> lines;
     ArrayList<ArrayList<String>> InfoFields;
+    final int DELTA = 25;
+    StringUtil checks;
 
     public void init(ArrayList<Line> lines)
         {
@@ -19,8 +27,22 @@ public class LocFields {
         }
     private void sortLines()
         {
-            for(Line l : lines)
+            int currY = 0;
+            int row =  0;
+            for(int i = 0 ; i < lines.size() ; ++i)
                 {
+                    Line check = lines.get(i);
+                        if(Math.abs(currY-check.getCornerPoints()[0].y)<DELTA && currY>0)
+                            {
+                                currY = check.getCornerPoints()[0].y;
+                                InfoFields.get(row).add(check.toString().trim());
+                            }
+                        else
+                            {
+                                currY = check.getCornerPoints()[0].y;
+                                InfoFields.add(new ArrayList<String>());
+                                InfoFields.get(row++).add(check.toString().trim());
+                            }
 
                 }
         }
